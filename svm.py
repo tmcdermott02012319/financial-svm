a23b7dc1f4a2b115e6e1fc4b75a129fc56755e42
tmcdermott02012319 <tristan_mcdermott@student.uml.edu>
2024-11-23 18:53:16 -0500

    Added SMOTE and hyperparam optimization

--- svm.py
+++ svm.py
@@ -1,7 +1,9 @@
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
-from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import balanced_accuracy_score, precision_score, recall_score, f1_score
+from imblearn.over_sampling import SMOTE
 
 def load_dataset():
 	with open("FinancialPhraseBank-v1.0/Sentences_50Agree.txt", "r", encoding = "latin1") as file:
@@ -15,22 +17,33 @@ def load_dataset():
 	return x_train, x_test, x_val, y_train, y_test, y_val
 
 def extract_features(x_train):
-	vectorizer = TfidfVectorizer()
+	vectorizer = TfidfVectorizer(ngram_range=(1, 1), min_df=10, max_df=0.5, sublinear_tf=True)
 	x_train = vectorizer.fit_transform(x_train)
 	return x_train, vectorizer
 
-def train_model(x_train, y_train):
-	model = LinearSVC()
-	return model.fit(x_train, y_train)
+def train_model(x_train, y_train, x_val, y_val):
+	model = SVC(class_weight="balanced")
+	params = {
+		"C": [0.1, 1, 10],
+		"kernel": ["linear", "poly", "rbf", "sigmoid"],
+		"gamma": [0.1, 1, "scale", "auto"]
+	}
+	optimizer = GridSearchCV(model, params, scoring="balanced_accuracy")
+	optimizer.fit(x_val, y_val)
+	return optimizer.best_estimator_.fit(x_train, y_train)
 
 def evaluate_model(model, x_test, y_test):
 	y_pred = model.predict(x_test)
-	print(classification_report(y_test, y_pred)) # TODO: balanced accuracy
+	print("Balanced accuracy:", balanced_accuracy_score(y_test, y_pred))
+	print("Precision:", precision_score(y_test, y_pred, average="weighted"))
+	print("Recall:", recall_score(y_test, y_pred, average="weighted"))
+	print("F1:", f1_score(y_test, y_pred, average="weighted"))
 
 if __name__ == "__main__":
 	x_train, x_test, x_val, y_train, y_test, y_val = load_dataset()
-	x_train, vectorizer = extract_features(x_train) # TODO: over/undersampling
+	x_train, vectorizer = extract_features(x_train)
 	x_test = vectorizer.transform(x_test)
 	x_val = vectorizer.transform(x_val)
-	model = train_model(x_train, y_train)
+	x_res, y_res = SMOTE().fit_resample(x_train, y_train)
+	model = train_model(x_res, y_res, x_val, y_val)
 	evaluate_model(model, x_test, y_test)
