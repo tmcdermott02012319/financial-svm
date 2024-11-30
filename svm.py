63e5f076dea1001b3e8c17e529fb0fe3d536ff85
tmcdermott02012319 <tristan_mcdermott@student.uml.edu>
2024-11-29 21:31:44 -0500

    Rewrote the whole thing

--- svm.py
+++ svm.py
@@ -1,57 +1,53 @@
 from sklearn.model_selection import train_test_split
+from imblearn.pipeline import make_pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import MaxAbsScaler
+from imblearn.over_sampling import SMOTE
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import balanced_accuracy_score, precision_score, recall_score, f1_score
-from sklearn.preprocessing import MaxAbsScaler
-from imblearn.over_sampling import SMOTE
+from pickle import load, dump
 
-def load_dataset():
-	with open("FinancialPhraseBank-v1.0/Sentences_50Agree.txt", "r", encoding = "latin1") as file:
-		data = file.read()
-	data = [line.split("@") for line in data.split("\n") if line]
-	x = [line[0] for line in data]
-	y = [line[1] for line in data]
-	# train = 60%, test = 20%, val = 20%
-	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-	x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25)
-	return x_train, x_test, x_val, y_train, y_test, y_val
+with open("FinancialPhraseBank-v1.0/Sentences_AllAgree.txt", "r", encoding = "latin1") as file: # no idea what encoding it actually is but utf-8 doesnt work
+	data = file.read()
 
-def extract_features(x_train):
-	vectorizer = TfidfVectorizer(ngram_range=(1, 1), min_df=10, max_df=0.5, sublinear_tf=True)
-	x_train = vectorizer.fit_transform(x_train)
-	return x_train, vectorizer
+data = [line.split("@") for line in data.split("\n") if line]
+x = [line[0] for line in data]
+y = [line[1] for line in data]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0) # train = 80%, test = 20%
 
-def train_model(x_train, y_train, x_val, y_val):
-	model = SVC(class_weight="balanced")
+try:
+	with open("model.pkl", "rb") as file:
+		model = load(file)
+except FileNotFoundError:
+	pipeline = make_pipeline(
+		TfidfVectorizer(),
+		MaxAbsScaler(),
+		SMOTE(),
+		SVC(),
+	)
 	params = {
-		"C": [0.1, 1, 10],
-		"kernel": ["linear", "poly", "rbf", "sigmoid"],
-		"gamma": [0.1, 1, "scale", "auto"]
+		"tfidfvectorizer__ngram_range": [(1, 1), (1, 2)],
+		"tfidfvectorizer__min_df": [0.001, 0.01, 0.1],
+		"tfidfvectorizer__max_df": [0.5, 0.75, 1.0],
+		"tfidfvectorizer__sublinear_tf": [True, False],
+		"smote__k_neighbors": [1, 2, 5, 10],
+		"smote__random_state": [0],
+		"svc__C": [0.1, 1, 10],
+		"svc__kernel": ["linear", "poly", "rbf"],
+		"svc__gamma": ["scale", "auto"],
+		"svc__class_weight": ["balanced"],
+		"svc__random_state": [0],
 	}
-	optimizer = GridSearchCV(model, params, scoring="balanced_accuracy")
-	optimizer.fit(x_val, y_val)
-	return optimizer.best_estimator_.fit(x_train, y_train)
+	grid = GridSearchCV(pipeline, params, scoring="balanced_accuracy", n_jobs=-1, cv=3, verbose=3) # due to cross validation the validation set goes unused
+	grid.fit(x_train, y_train)
+	model = grid.best_estimator_
+	print("Best params:", grid.best_params_)
+	with open("model.pkl", "wb") as file:
+		dump(model, file)
 
-def evaluate_model(model, x_test, y_test):
-	y_pred = model.predict(x_test)
-	print("Balanced accuracy:", balanced_accuracy_score(y_test, y_pred))
-	print("Precision:", precision_score(y_test, y_pred, average="weighted"))
-	print("Recall:", recall_score(y_test, y_pred, average="weighted"))
-	print("F1:", f1_score(y_test, y_pred, average="weighted"))
-
-if __name__ == "__main__":
-	x_train, x_test, x_val, y_train, y_test, y_val = load_dataset()
-	x_train, vectorizer = extract_features(x_train)
-	x_test = vectorizer.transform(x_test)
-	x_val = vectorizer.transform(x_val)
-	scaler = MaxAbsScaler() # recommended for sparse matrices instead of StandardScaler
-	x_train = scaler.fit_transform(x_train)
-	x_test = scaler.fit_transform(x_test)
-	x_val = scaler.fit_transform(x_val)
-	sampler = SMOTE()
-	x_train, y_train = sampler.fit_resample(x_train, y_train)
-	x_test, y_test = sampler.fit_resample(x_test, y_test)
-	x_val, y_val = sampler.fit_resample(x_val, y_val)
-	model = train_model(x_train, y_train, x_val, y_val)
-	evaluate_model(model, x_test, y_test)
+y_pred = model.predict(x_test)
+print("Balanced accuracy:", balanced_accuracy_score(y_test, y_pred))
+print("Precision:", precision_score(y_test, y_pred, average="weighted"))
+print("Recall:", recall_score(y_test, y_pred, average="weighted"))
+print("F1:", f1_score(y_test, y_pred, average="weighted"))
