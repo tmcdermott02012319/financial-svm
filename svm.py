7311165f9e8ce44271aa585f4a99d453d309dc7c
tmcdermott02012319 <tristan_mcdermott@student.uml.edu>
2024-12-08 17:21:55 -0500

    Set random state

--- svm.py
+++ svm.py
@@ -8,14 +8,14 @@ from pickle import load, dump
 
 
 def load_dataset():
-	with open("FinancialPhraseBank-v1.0/Sentences_50Agree.txt", "r", encoding = "latin1") as file:
+	with open("FinancialPhraseBank-v1.0/Sentences_AllAgree.txt", "r", encoding = "latin1") as file:
 		data = file.read()
 	data = [line.split("@") for line in data.split("\n") if line]
 	x = [line[0] for line in data]
 	y = [line[1] for line in data]
 	# train = 60%, test = 20%, val = 20%
-	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y)
-	x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25, stratify=y_train)
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y, random_state=0)
+	x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.25, stratify=y_train, random_state=0)
 	return x_train, x_test, x_val, y_train, y_test, y_val
 
 
@@ -28,14 +28,17 @@ def extract_features(x_train):
 	return x_train, vectorizer
 
 def train_model(x_train, y_train, x_val, y_val):
-	_model = SVC(class_weight="balanced", probability=True)
+	_model = SVC()
 	params = {
 		"C": [.001, .01, 0.1, 1, 10, 100],
 		"kernel": ["linear", "poly", "rbf", "sigmoid"],
 		"gamma": [0.1, 1, "scale", "auto"],
-		"degree": [2, 3, 4]
+		"degree": [2, 3, 4],
+		"class_weight": ["balanced"],
+		"probability": [True],
+		"random_state": [0]
 	}
-	optimizer = GridSearchCV(_model, params, scoring="balanced_accuracy", n_jobs=-1)
+	optimizer = GridSearchCV(_model, params, scoring="balanced_accuracy", n_jobs=-1, verbose=3)
 	optimizer.fit(x_val, y_val)
 	model = optimizer.best_estimator_.fit(x_train, y_train)
 	with open("model.pkl", "wb") as file:
@@ -64,6 +67,6 @@ if __name__ == "__main__":
 		x_train, vectorizer = extract_features(x_train)
 		x_test = vectorizer.transform(x_test)
 		x_val = vectorizer.transform(x_val)
-		x_res, y_res = SMOTE().fit_resample(x_train, y_train)
+		x_res, y_res = SMOTE(random_state=0).fit_resample(x_train, y_train)
 		model = train_model(x_res, y_res, x_val, y_val)
 	evaluate_model(model, x_test, y_test)
